80f87cd7e929a25dde7011ad78894fc635b54b5a
Lichard Baliuag <baliuag_lichard@yahoo.com>
2019-09-26 14:54:21 +1200

    Initial commit for Github Emojis project

---
+++ GithubEmojis/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GithubEmojis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IGithubEmojiService _emojiService;
+
+        public IndexModel(IGithubEmojiService emojiSvc)
+        {
+            _emojiService = emojiSvc;
+        }
+
+        public IList<Emoji> emojis {
+            get;
+            set;
+        }
+
+        public async Task OnGet()
+        {
+            emojis = await _emojiService.GetEmojis();
+        }
+    }
+}
